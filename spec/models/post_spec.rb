14cca5d839d26bf25c925b4e6d86d33e60665a03
Kirill Nagaqcev <nagavi25599@gmail.com>
2021-05-19 21:23:18 +0700

    Fix Rubocop 2

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context "validation tests" do
-    it "ensures title is present" do
-      post = Post.new(body: "Test", summary: "Test")
+  describe 'Validation tests' do
+    it 'ensures title is present' do
+      post = described_class.new(body: 'Test', summary: 'Test')
       expect(post.valid?).to eq(false)
     end
-    
-    it "ensures body is present" do
-      post = Post.new(title: "Test", summary: "Test")
+
+    it 'ensures body is present' do
+      post = described_class.new(title: 'Test', summary: 'Test')
       expect(post.valid?).to eq(false)
     end
-    
-    it "ensures summary is present" do
-      post = Post.new(title: "Test", body: "Test")
+
+    it 'ensures summary is present' do
+      post = described_class.new(title: 'Test', body: 'Test')
       expect(post.valid?).to eq(false)
     end
   end
